0b7b48cf32a2873e0b8b5a5d3011216430f89f07
SPVJ <42379752+SPVJ@users.noreply.github.com>
2019-05-28 21:24:48 +0700

    Update login.py

--- src/login.py
+++ src/login.py
@@ -5,13 +5,19 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker,relationships
 from ui_py.Login_UI import Ui_Form
-
+from src.loginSystem import LoginSystem
 class LoginArea(QWidget):
     def __init__(self):
         QWidget.__init__(self,None)
-
+        self.loginControl = LoginSystem()
         self.ui = Ui_Form()
         self.ui.setupUi(self)
+
+        self.ui.reg_b.clicked.connect(self.register)
+
+
+    def reg(self):
+
 
 
 
