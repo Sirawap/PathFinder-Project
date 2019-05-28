63b80b02ef170920639ee5f0bea866787c0407fc
SPVJ <42379752+SPVJ@users.noreply.github.com>
2019-05-28 18:38:31 +0700

    Update loginSystem.py

--- src/loginSystem.py
+++ src/loginSystem.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker,relationship
 from src.accountClasses import UserAcc,CompanyAcc
-base = declarative_base()
+
 engine = create_engine("mysql+pymysql://root:pathfinder@127.0.0.1:3307/pathfinderDB",echo = True)
 
 class LoginSystem():
