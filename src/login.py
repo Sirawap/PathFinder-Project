ba3374744c7115b6a0b3af4f59ab9cdc37cfbb1e
SPVJ <42379752+SPVJ@users.noreply.github.com>
2019-05-28 17:15:36 +0700

    Update login.py

--- src/login.py
+++ src/login.py
@@ -2,3 +2,5 @@ from PySide2 import QtWidgets,QtCore,QtGui
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker,relationships
+from ui_py.Login_UI import Ui_Form
+
