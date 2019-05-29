4f842589b8961fa6176da102c1038752abc9aa55
pollakit <60090031@kmitl.ac.th>
2019-05-29 12:24:56 +0700

    Connect login to login.py

--- src/Company_Main_GUI.py
+++ src/Company_Main_GUI.py
@@ -3,11 +3,11 @@ from PySide2.QtWidgets import *
 from PySide2.QtCore import *
 import ui_py.User_main_screen
 
-class User_Main_GUI(QMainWindow):
+class Comp_Main_GUI(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self,None)
 
-        self.ui = ui_py.User_main_screen.Ui_Form()
+        self.ui = ui_py.User_main_screen.Ui_MainWindow()
         self.ui.setupUi(self)
 
 
