29f1d2d7bb6fe1684970fdc7ec1a218ecf006818
pollakit <60090031@kmitl.ac.th>
2019-05-29 17:37:13 +0700

    re เยอะ

--- src/EditComp_Profile_GUI.py
+++ src/EditComp_Profile_GUI.py
@@ -1,11 +1,11 @@
 import sys
 from PySide2.QtWidgets import *
 from PySide2.QtCore import *
-import ui_py.Company_Edit_Profile2
+import ui_py.Company_Edit_Profile
 
 class Edit_Company_Profile_GUI(QWidget):
     def __init__(self):
         QWidget.__init__(self,None)
 
-        self.ui = ui_py.Company_Edit_Profile2.Ui_Form()
+        self.ui = ui_py.Company_Edit_Profile.Ui_Form()
         self.ui.setupUi(self)
