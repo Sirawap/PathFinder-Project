86a3037f921c93b8dfa79b8b3dceb6f5ca5bec3f
SPVJ <42379752+SPVJ@users.noreply.github.com>
2019-05-29 18:30:45 +0700

    company reg

--- src/Company_Reg_GUI.py
+++ src/Company_Reg_GUI.py
@@ -2,13 +2,38 @@ import sys
 from PySide2.QtWidgets import *
 from PySide2.QtCore import *
 import ui_py.Company_Registeration
+from src.loginSystem import LoginSystem
+from src.Company import Company
+from src.accountClasses import CompanyAcc
 
 class Company_GUI(QWidget):
     def __init__(self):
         QWidget.__init__(self,None)
-
+        self.loginControl = LoginSystem()
         self.user_ui = ui_py.Company_Registeration.Ui_Form()
         self.user_ui.setupUi(self)
+
+        self.user_ui.confirm_b.clicked.connect(self.com_reg)
+
+    def com_reg(self):
+        cname = self.user_ui.register_company_name.text()
+        email = self.user_ui.register_email.text()
+        tel = self.user_ui.register_tel.text()
+
+        usr = self.user_ui.register_username.text()
+        pwd = self.user_ui.register_password.text()
+        rpwd = self.user_ui.register_rePassword.text()
+
+        if cname == "" or email == "" or tel == "" or usr =="" or pwd == "" or rpwd =="":
+            self.user_ui.error_label.setText("Please fill (*) info")
+            return
+        if pwd != rpwd:
+            self.user_ui.error_label.setText("Passwords dont match!")
+            return
+        else:
+            rettext = self.loginControl.createCompany(usr,pwd,cname,email,tel)
+            self.user_ui.error_label.setText(rettext)
+            return
 
 
 
--- src/loginSystem.py
+++ src/loginSystem.py
@@ -3,24 +3,29 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker,relationship
 from src.accountClasses import UserAcc,CompanyAcc
 from src.Jobseeker import Jobseeker
+from src.Company import Company
 engine = create_engine("mysql+pymysql://root:pathfinder@127.0.0.1:3307/pathfinderDB",echo = True)
 
 class LoginSystem():
     def __init__(self):
         pass
 
-    def createCompany(self,usr,pwd):
+    def createCompany(self,usr,pwd,cname,email,tel):
         Session = sessionmaker(bind = engine)
         session = Session()
         ca = CompanyAcc(username = usr, password = pwd)
+        c = Company(username = usr, companyName = cname,email=email,tel=tel)
         checkExist = session.query(CompanyAcc).filter(CompanyAcc.username == ca.username)
         boolExist = session.query(checkExist.exists()).scalar()
-        if boolExist:
+        checkCExist = session.query(Company).filter(Company.companyName == c.companyName)
+        cboolExist = session.query(checkCExist.exists()).scalar()
+        if boolExist or cboolExist :
             session.commit()
             session.close()
-            return "Username for Company already exists"
+            return "Username or company name already exists"
         else:
             session.add(ca)
+            session.add(c)
             session.commit()
             session.close()
             return "Done Register !"
