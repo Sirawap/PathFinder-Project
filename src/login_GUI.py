09a670607188ab4dfbb71e56a1479cd037da7dbe
SPVJ <42379752+SPVJ@users.noreply.github.com>
2019-05-30 22:14:36 +0700

    Update login_GUI.py

--- src/login_GUI.py
+++ src/login_GUI.py
@@ -2,9 +2,15 @@ import sys
 from PySide2.QtGui import *
 from PySide2.QtCore import *
 from PySide2.QtWidgets import *
+from src.loginSystem import LoginSystem
+from src.Company import Company
+from src.Jobseeker import Jobseeker
+from src.accountClasses import CompanyAcc
+from src.accountClasses import UserAcc
 # from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 # from sqlalchemy.ext.declarative import declarative_base
-# from sqlalchemy.orm import sessionmaker,relationships
+# from sqlalchemy.orm import sessionmaker,relationship
+# s
 from ui_py.Login_UI import Ui_Form
 #from src.loginSystem import LoginSystem
 # class LoginArea(QWidget):
@@ -32,7 +38,7 @@ class Login_GUI(QWidget):
         self.ui.setupUi(self)
         self.usr = ''
         self.psd = ''
-
+        self.loginControl = LoginSystem()
         self.ui.reg_b.clicked.connect(self.checkRadioR)
         self.ui.login_b.clicked.connect(self.checkRadioM)
 
@@ -63,9 +69,15 @@ class Login_GUI(QWidget):
         self.close()
 
     def openUser_Main_UI(self):
-        self.__userM_ui = src.User_Main_GUI.User_Main_GUI() ##
-        self.__userM_ui.show()
-        self.close()
+
+        response = self.loginControl.loginUser(self.ui.username.text(),self.ui.password.text())
+        if type(response) == str :
+            self.ui.label_3.setText(response)
+            return
+        else:
+            self.__userM_ui = src.User_Main_GUI.User_Main_GUI(response)  ##
+            self.__userM_ui.show()
+            self.close()
 
     def checkInput(self,usr,psd):
         unvalidInput = ["!","'","/",".","=","!"]
@@ -75,13 +87,10 @@ class Login_GUI(QWidget):
                     raise Exception
             self.usr = usr
             self.psd = psd
-
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     w = Login_GUI()
     w.show()
 
     sys.exit(app.exec_())
-
-
 
