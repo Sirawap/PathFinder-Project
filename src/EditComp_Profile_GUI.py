d40aad89980910c2ff958e71f838d8e8d877ec13
Sirawap <45166334+Sirawap@users.noreply.github.com>
2019-05-31 12:22:11 +0700

    a

--- src/EditComp_Profile_GUI.py
+++ src/EditComp_Profile_GUI.py
@@ -1,9 +1,19 @@
+import sys
 from PySide2.QtWidgets import *
+from PySide2.QtCore import *
 import ui_py.Company_Edit_Profile
 
 class Edit_Company_Profile_GUI(QWidget):
     def __init__(self):
         QWidget.__init__(self,None)
-
         self.ui = ui_py.Company_Edit_Profile.Ui_Form()
         self.ui.setupUi(self)
+        self.ui.pushButton_confirm_profile.clicked.connect(self.user_edit)
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    w = Edit_Company_Profile_GUI()
+    w.show()
+    sys.exit(app.exec_())
