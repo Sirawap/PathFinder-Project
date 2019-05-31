c78ec4e95e38fd7ed38a9c78455cad99fb5d9385
pollakit <60090031@kmitl.ac.th>
2019-05-31 12:44:51 +0700

    Update EditComp_Profile_GUI.py

--- src/EditComp_Profile_GUI.py
+++ src/EditComp_Profile_GUI.py
@@ -7,4 +7,18 @@ class Edit_Company_Profile_GUI(QWidget):
 
         self.ui = ui_py.Company_Edit_Profile.Ui_Form()
         self.ui.setupUi(self)
-        self.ui.buttonBox.
+        self.ui.pushButton_OK.clicked.connect(self.commitChange)
+        self.ui.pushButton_Cancle.clicked.connect(self.cancleChange)
+        self.ui.pushButton.clicked.connect(self.chooseLogo)
+
+    def commitChange(self):
+        ##please add more
+        self.close()
+
+    def cancleChange(self):
+        ##please add more
+        self.close()
+
+    def chooseLogo(selfself):
+        ##please add more
+        pass
