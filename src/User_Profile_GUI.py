878c2c01b47320ece36f1039d5f4d9859fb0ba90
pollakit <60090031@kmitl.ac.th>
2019-05-31 17:33:48 +0700

    company chisai change and new ui

--- src/User_Profile_GUI.py
+++ src/User_Profile_GUI.py
@@ -2,10 +2,16 @@ import sys
 from PySide2.QtWidgets import *
 from PySide2.QtCore import *
 import ui_py.User_View_profile_UI
+import src.Jobseeker
 
 class View_User_Profile_GUI(QWidget):
-    def __init__(self):
+    def __init__(self,usr = src.Jobseeker.Jobseeker()):
         QWidget.__init__(self,None)
 
         self.ui = ui_py.User_View_profile_UI.Ui_Form()
         self.ui.setupUi(self)
+
+        self.usr = usr
+        self.ui.label_Firstname.setText(self.usr.fname)
+        self.ui.label_Surname.setText(self.usr.surname)
+        self.ui.label_Surname.setText(self.usr.surname)
