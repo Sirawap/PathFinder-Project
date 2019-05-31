df5f2da88166062947b13568c671cbe85f49b24c
Sirawap <45166334+Sirawap@users.noreply.github.com>
2019-05-31 17:30:35 +0700

    Update EditComp_Profile_GUI.py

--- src/EditComp_Profile_GUI.py
+++ src/EditComp_Profile_GUI.py
@@ -13,7 +13,7 @@ class Edit_Company_Profile_GUI(QWidget):
     def comp_edit(self):
         name = self.ui.lineEdit_comp_name.text()
         tel = self.ui.lineEdit_tel.text()
-        web = self.ui.lineEdit_web.text()
+        mail = self.ui.lineEdit_web.text()
         no = self.ui.lineEdit_houseNo.text()
         soi = self.ui.lineEdit_soi.text()
         street = self.ui.lineEdit_street.text()
@@ -22,43 +22,14 @@ class Edit_Company_Profile_GUI(QWidget):
         province = self.ui.lineEdit_state.text()
         zip = self.ui.lineEdit_zip.text()
         business = self.ui.comboBox_Business.currentText()
-        '''business = ''
-        if self.ui.checkBox_14.isChecked() :
-            business += ","+self.ui.checkBox_14.text()
-        if self.ui.checkBox_15.isChecked():
-            business += ","+self.ui.checkBox_15.text()
-        if self.ui.checkBox_17.isChecked():
-            business += ","+self.ui.checkBox_17.text()
-        if self.ui.checkBox_21.isChecked():
-            business += ","+self.ui.checkBox_21.text()
-        if self.ui.checkBox_22.isChecked():
-            business += ","+self.ui.checkBox_22.text()
-        if self.ui.checkBox_23.isChecked():
-            business += ","+self.ui.checkBox_23.text()
-        if self.ui.checkBox_24.isChecked():
-            business += ","+self.ui.checkBox_24.text()
-        if self.ui.checkBox_26.isChecked():
-            business += ","+self.ui.checkBox_26.text()
-        if self.ui.checkBox_29.isChecked():
-            business += ","+self.ui.checkBox_29.text()
-        if self.ui.checkBox_31.isChecked():
-            business += ","+self.ui.checkBox_31.text()
-        if self.ui.checkBox_33.isChecked():
-            business += ","+self.ui.checkBox_33.text()
-        if self.ui.checkBox_36.isChecked():
-            business += ","+self.ui.checkBox_36.text()
-        if self.ui.checkBox_37.isChecked():
-            business += ","+self.ui.checkBox_37.text()
-        if self.ui.checkBox_39.isChecked():
-            business += ","+self.ui.checkBox_39.text()
-        if self.ui.checkBox_40.isChecked():
-            business += ","+self.ui.checkBox_40.text()
-        if self.ui.checkBox_41.isChecked():
-            business += ","+self.ui.checkBox_41.text()
-        if self.ui.checkBox_47.isChecked():
-            business += ","+self.ui.checkBox_47.text()
-        business = business[1:]
-        print(business)'''
+        if name == '':
+            print("Please enter your company name")
+        if tel == '' or mail == '':
+            print("Please enter your company contact")
+        if no == '' or street== '' or city == '' or province == '' or zip == '':
+            print("Please filled your company address")
+        if business == "  -- None --":
+            print("Please select business")
         print("F for reespect")
 
 if __name__ == "__main__":
