da02b3c43414c57629ad766a43d5894499c4a364
pollakit <60090031@kmitl.ac.th>
2019-05-31 16:52:48 +0700

    company view profile remove logo

--- src/Company_Profile_GUI.py
+++ src/Company_Profile_GUI.py
@@ -3,8 +3,13 @@ import ui_py.Company_view_profile
 import src.Company
 
 class View_Company_Profile_GUI(QWidget):
-    def __init__(self):
+    def __init__(self,company = src.Company.Company()):
         QWidget.__init__(self,None)
+
+        self.mainCompany = company
 
         self.ui = ui_py.Company_view_profile.Ui_Form()
         self.ui.setupUi(self)
+
+        self.ui.label_5.setText(self.mainCompany.getCompanyName())
+        self.ui.label_5.setText(self.mainCompany.getCompanyName())
