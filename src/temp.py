882f424aabff9242c7408160608fe5f1e6fcb1db
pollakit <60090031@kmitl.ac.th>
2019-06-01 23:38:59 +0700

    table color

--- src/temp.py
+++ src/temp.py
@@ -18,7 +18,8 @@ class temp(QWidget):
         # model = QStandardItemModel()
         # model.setHorizontalHeaderLabels(['No','Name','Surname','age','tel'])
 
-        style = "::section {""background-color: lightblue; }"
+        style = "::section {""background-color: gray;" \
+                "color: red; }"
         self.ui.tableWidget.horizontalHeader().setStyleSheet(style)
         self.ui.tableWidget.setColumnCount(5)
         self.ui.tableWidget.setColumnWidth(4,500)
