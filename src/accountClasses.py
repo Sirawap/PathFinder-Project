73d451f8d97b6e79c6d937c6a01536dc42044d7d
SPVJ <42379752+SPVJ@users.noreply.github.com>
2019-06-02 14:23:32 +0700

    Update accountClasses.py

--- src/accountClasses.py
+++ src/accountClasses.py
@@ -3,12 +3,6 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker,relationship
 base = declarative_base()
 engine = create_engine("mysql+pymysql://root:pathfinder@127.0.0.1:3307/pathfinderDB",echo = True)
-class Account(base):
-    __tablename__ = "userTest"
-
-    id = Column("userid", primary_key=True)
-    name = Column("name", String, unique=False)
-    age = Column("age", Integer, unique=False)
 
 class UserAcc(base):
     __tablename__ = "seekerLogin"
